5201f4900793a67d8e06880b9a0740eca66eee5f
huy <thuyhuy333@gmail.com>
2021-04-17 16:55:11 +0700

    remove echo

--- index.php
+++ index.php
@@ -4,6 +4,6 @@ header("Refresh: 5; URL=$url1");
 $imagesDir = 'images/';
 $images = glob($imagesDir . '*.{jpg,jpeg,png,gif}', GLOB_BRACE);
 foreach ($images as $img) {
-echo "<img src='$img' width='300' height='300'/> ";
+
 }
 ?>
